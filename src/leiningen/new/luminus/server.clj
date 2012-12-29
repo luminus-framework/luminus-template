d115cb7a30592d5937127c868604cc1aab8063bb
Chris Allen <cma@bitemyapp.com>
2012-12-28 22:22:44 -0800

    Documented wrappers, sorta, documented gen-class.

--- src/leiningen/new/luminus/server.clj
+++ src/leiningen/new/luminus/server.clj
@@ -4,6 +4,9 @@
         [ring.middleware file-info file]
         bultitude.core)
   (:gen-class))
+; gen-class propagates, you don't need it outside of server.clj
+; (:gen-class) must be in the ns in order for lein uberjar to work.
+; Enables you to byte-compile everything into a single jar/war
 
 (defn wrap-reload-if-available [handler]
   (if (some #{'ring.middleware.reload} (namespaces-on-classpath))
@@ -18,7 +21,9 @@
   ; rather than having its own copy. When the root binding
   ; changes, the server picks it up without having to restart.
   (-> #'app
+    ; Makes static assets in $PROJECT_DIR/resources/public/ available.
     (wrap-file "resources")
+    ; Content-Type, Content-Length, and Last Modified headers for files in body
     (wrap-file-info)
     (wrap-reload-if-available)))
 
@@ -28,5 +33,3 @@
     (run-jetty (get-handler) {:join? false :port port})
     (println "Server started on port [" port "].")
     (println (str "You can view the site at http://localhost:" port))))
-
-
