6aedb05dfc3b5b83dbf415a874f75b1fbccd9eac
Yogthos <dmitri.sotnikov@gmail.com>
2013-01-03 13:37:21 -0500

    bumped version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "0.2.1"
+(defproject luminus/lein-template "0.2.2"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "Eclipse Public License"
