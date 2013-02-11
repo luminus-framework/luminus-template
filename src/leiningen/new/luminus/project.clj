766fcfaf2d35acad90854477ab1dcd70d6c8572c
Yogthos <dmitri.sotnikov@gmail.com>
2013-02-10 22:04:49 -0500

    updated dependencies added log4j.xml for Korma

--- src/leiningen/new/luminus/project.clj
+++ src/leiningen/new/luminus/project.clj
@@ -2,20 +2,20 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [lib-noir "0.3.5"]
+                 [lib-noir "0.3.6"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.2"]
-                 [ring-server "0.2.7"]
-                 [com.taoensso/timbre "1.2.0"]
+                 [ring-server "0.2.7"]                 
+                 [com.taoensso/timbre "1.5.1"]
                  [com.taoensso/tower "1.2.0"]
-                 [markdown-clj "0.9.19"]]
+                 [markdown-clj "0.9.19"]]  
   :plugins [[lein-ring "0.8.2"]]
   :ring {:handler {{name}}.handler/war-handler
          :init    {{name}}.handler/init
-         :destroy {{name}}.handler/destroy}
+         :destroy {{name}}.handler/destroy}  
   :profiles
-  {:production {:ring {:open-browser? false
-                       :stacktraces?  false
+  {:production {:ring {:open-browser? false 
+                       :stacktraces?  false 
                        :auto-reload?  false}}
    :dev {:dependencies [[ring-mock "0.1.3"]
                         [ring/ring-devel "1.1.8"]]}})
