e37293adb4769fe64db99dd2bff5b8f48f81e33e
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-04-27 19:40:21 -0300

    Update project.clj

--- src/leiningen/new/luminus/project.clj
+++ src/leiningen/new/luminus/project.clj
@@ -4,12 +4,12 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [lib-noir "0.5.0"]
                  [compojure "1.1.5"]
-                 [ring-server "0.2.7"]
+                 [ring-server "0.2.8"]
                  [clabango "0.5"]
                  [com.taoensso/timbre "1.5.2"]
                  [com.taoensso/tower "1.5.1"]
                  [markdown-clj "0.9.19"]]
-  :plugins [[lein-ring "0.8.3"]]
+  :plugins [[lein-ring "0.8.5"]]
   :ring {:handler {{name}}.handler/war-handler
          :init    {{name}}.handler/init
          :destroy {{name}}.handler/destroy}
