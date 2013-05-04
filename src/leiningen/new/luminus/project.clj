41e85436fb5ea621e9f6791987615e7971f466a6
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-05-04 18:38:43 -0300

    Update project.clj

--- src/leiningen/new/luminus/project.clj
+++ src/leiningen/new/luminus/project.clj
@@ -6,7 +6,7 @@
                  [compojure "1.1.5"]
                  [ring-server "0.2.8"]
                  [clabango "0.5"]
-                 [com.taoensso/timbre "1.5.2"]
+                 [com.taoensso/timbre "1.6.0"]
                  [com.taoensso/tower "1.5.1"]
                  [markdown-clj "0.9.19"]]
   :plugins [[lein-ring "0.8.5"]]
