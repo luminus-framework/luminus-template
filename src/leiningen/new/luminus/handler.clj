a4ea3e2a0ea99fbae2cb47df1582c41e323619d8
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-05-04 18:40:14 -0300

    Update handler.clj

--- src/leiningen/new/luminus/handler.clj
+++ src/leiningen/new/luminus/handler.clj
@@ -2,11 +2,16 @@
   (:use {{name}}.routes.home
         compojure.core)
   (:require [noir.util.middleware :as middleware]
-            [compojure.route :as route]))
+            [compojure.route :as route]
+            [taoensso.timbre :as timbre]
+            [com.postspectacular.rotor :as rotor]))
 
 (defroutes app-routes
   (route/resources "/")
   (route/not-found "Not Found"))
+
+(defn destroy []
+  (timbre/info "picture-gallery is shutting down"))
 
 (defn init
   "init will be called once when
@@ -14,13 +19,25 @@
    an app server such as Tomcat
    put any initialization code here"
   []
-  (println "{{name}} started successfully..."))
+  (timbre/set-config!
+    [:appenders :rotor]
+    {:min-level :error
+     :enabled? true
+     :async? false ; should be always false for rotor
+     :max-message-per-msecs nil
+     :fn rotor/append})
+  
+  (timbre/set-config!
+    [:shared-appender-config :rotor]
+    {:path "error.log" :max-size 10000 :backlog 10})
+  
+  (timbre/info "{{name}} started successfully"))
 
 (defn destroy
   "destroy will be called when your application
    shuts down, put any clean up code here"
   []
-  (println "shutting down..."))
+  (timbre/info "{{name}} is shutting down..."))
 
 ;;append your application routes to the all-routes vector
 (def all-routes [home-routes app-routes])
