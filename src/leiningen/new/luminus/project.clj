53f1d7d7e1afbc6f376c931c46c76305d0ef8074
Yogthos <dmitri.sotnikov@gmail.com>
2013-09-05 23:19:53 -0400

    bumped versions

--- src/leiningen/new/luminus/project.clj
+++ src/leiningen/new/luminus/project.clj
@@ -2,14 +2,14 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [lib-noir "0.6.8"]
+                 [lib-noir "0.6.9"]
                  [compojure "1.1.5"]
                  [ring-server "0.2.8"]
                  [selmer "0.4.2"]
                  [com.taoensso/timbre "2.6.1"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "1.7.1"]
-                 [markdown-clj "0.9.30"]]
+                 [markdown-clj "0.9.31"]]
   :plugins [[lein-ring "0.8.6"]]
   :ring {:handler {{name}}.handler/war-handler
          :init    {{name}}.handler/init
