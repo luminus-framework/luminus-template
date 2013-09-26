b27820a0133ee93710f9501a89d153435151ce0a
Yogthos <dmitri.sotnikov@gmail.com>
2013-09-26 09:32:28 -0400

    updated to set UTF-8 when rendering templates

--- src/leiningen/new/luminus/layout.clj
+++ src/leiningen/new/luminus/layout.clj
@@ -1,6 +1,6 @@
 (ns {{name}}.views.layout
   (:require [selmer.parser :as parser]
-            [ring.util.response :refer [response]])
+            [ring.util.response :refer [content-type response]])
   (:import compojure.response.Renderable))
 
 (def template-path "{{sanitized}}/views/templates/")
@@ -8,9 +8,11 @@
 (deftype RenderableTemplate [template params]
   Renderable
   (render [this request]
-    (->> (assoc params :servlet-context (:context request))
-         (parser/render-file (str template-path template))
-         response)))
+    (content-type
+      (->> (assoc params :servlet-context (:context request))
+        (parser/render-file (str template-path template))
+        response)
+      "text/html; charset=utf-8")))
 
 (defn render [template & [params]]
   (RenderableTemplate. template params))
--- src/leiningen/new/luminus/site/layout.clj
+++ src/leiningen/new/luminus/site/layout.clj
@@ -1,7 +1,7 @@
 (ns {{name}}.views.layout
   (:require [selmer.parser :as parser]
             [noir.session :as session]
-            [ring.util.response :refer [response]])
+            [ring.util.response :refer [content-type response]])
   (:import compojure.response.Renderable))
 
 (def template-path "{{sanitized}}/views/templates/")
@@ -9,11 +9,11 @@
 (deftype RenderableTemplate [template params]
   Renderable
   (render [this request]
-    (->> (assoc params
-                :servlet-context (:context request)
-                :user-id (session/get :user-id))
-         (parser/render-file (str template-path template))
-         response)))
+    (content-type
+      (->> (assoc params :servlet-context (:context request))
+        (parser/render-file (str template-path template))
+        response)
+      "text/html; charset=utf-8")))
 
 (defn render [template & [params]]
   (RenderableTemplate. template params))
