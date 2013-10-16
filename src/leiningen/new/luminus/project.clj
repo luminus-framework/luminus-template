b921232a22ddf47460a6676cc31205c59feee28d
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-10-16 19:00:31 -0400

    Update project.clj

--- src/leiningen/new/luminus/project.clj
+++ src/leiningen/new/luminus/project.clj
@@ -5,7 +5,7 @@
                  [lib-noir "0.7.1"]
                  [compojure "1.1.5"]
                  [ring-server "0.3.0"]
-                 [selmer "0.4.4"]
+                 [selmer "0.4.6"]
                  [com.taoensso/timbre "2.6.2"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "1.7.1"]
