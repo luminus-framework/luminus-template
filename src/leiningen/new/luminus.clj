67d6315155600644e8bcdf98d34939ce924bf62f
Yogthos <dmitri.sotnikov@gmail.com>
2013-11-06 19:30:57 -0500

    bumped up cljs version

--- src/leiningen/new/luminus.clj
+++ src/leiningen/new/luminus.clj
@@ -44,7 +44,7 @@
   (add-routes (sanitized-path "/handler.clj") 'cljs-routes)
   (add-dependencies project-file
                     ;;needed to get the latest version of ClojureScript until cljsbuild gets up to date
-                    ['org.clojure/clojurescript "0.0-1934"]
+                    ['org.clojure/clojurescript "0.0-1978"]
                     ['domina "1.0.2"]
                     ['prismatic/dommy "0.1.2"]
                     ['cljs-ajax "0.2.0"])
