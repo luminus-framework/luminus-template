70e18a77e4515ca5bedbd9c38db87607f91eeb78
Yogthos <dmitri.sotnikov@gmail.com>
2014-01-30 23:55:33 -0500

    updated timbre

--- src/leiningen/new/luminus/project.clj
+++ src/leiningen/new/luminus/project.clj
@@ -6,7 +6,7 @@
                  [compojure "1.1.6"]
                  [ring-server "0.3.1"]
                  [selmer "0.5.9"]
-                 [com.taoensso/timbre "2.7.1"]
+                 [com.taoensso/timbre "3.0.0"]
                  [com.postspectacular/rotor "0.1.0"]
                  [com.taoensso/tower "2.0.1"]
                  [markdown-clj "0.9.41"]
