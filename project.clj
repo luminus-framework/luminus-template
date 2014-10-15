04a6a30071c4d052c5af00b8e4721f6c2e8906e5
Yogthos <dmitri.sotnikov@gmail.com>
2014-10-15 09:07:07 -0400

    bumped up lib-noir

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "1.15.9"
+(defproject luminus/lein-template "1.16.0"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "MIT License"
--- src/leiningen/new/luminus/project.clj
+++ src/leiningen/new/luminus/project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [lib-noir "0.9.2"]
+                 [lib-noir "0.9.3"]
                  [ring-server "0.3.1"]
                  [selmer "0.7.2"]
                  [com.taoensso/timbre "3.3.1"]
