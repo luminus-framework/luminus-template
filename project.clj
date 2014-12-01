93b034e85f9e1b2a7af358b6b8eaa15ec69219f0
Yogthos <dmitri.sotnikov@gmail.com>
2014-12-01 00:22:15 -0500

    updated to set default content-type in dev mode

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject luminus/lein-template "1.16.8"
-  :description "a template for creating Luminus applications"
-  :url "https://github.com/yogthos/luminus-template"
-  :license {:name "MIT License"
-            :url "http://opensource.org/licenses/MIT"}
-  :eval-in-leiningen true
-  :dependencies [[leinjacker "0.2.0"]])
+(defproject luminus/lein-template "1.17.0"
+            :description "a template for creating Luminus applications"
+            :url "https://github.com/yogthos/luminus-template"
+            :license {:name "MIT License"
+                      :url "http://opensource.org/licenses/MIT"}
+            :eval-in-leiningen true
+            :dependencies [[leinjacker "0.2.0"]])
--- src/leiningen/new/luminus/middleware.clj
+++ src/leiningen/new/luminus/middleware.clj
@@ -2,6 +2,7 @@
   (:require [taoensso.timbre :as timbre]
             [selmer.parser :as parser]
             [environ.core :refer [env]]
+            [noir.response :refer [content-type]]
             [selmer.middleware :refer [wrap-error-page]]
             [prone.middleware :refer [wrap-exceptions]]
             [noir-exception.core :refer [wrap-internal-error]]))
@@ -11,9 +12,17 @@
     (timbre/debug req)
     (handler req)))
 
+(defn wrap-default-content-type [handler]
+  (fn [req]
+      (let [response (handler req)]
+           (if (and (map? response) (empty? (get-in response [:headers "Content-Type"])))
+             (content-type "text/html; charset=utf-8" response)
+             response))))
+
 (def development-middleware
   [wrap-error-page
-   wrap-exceptions])
+   wrap-exceptions
+   wrap-default-content-type])
 
 (def production-middleware
   [#(wrap-internal-error % :log (fn [e] (timbre/error e)))])
