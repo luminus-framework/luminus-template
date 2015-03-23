53302b405a10425df911e8560d6de280d379afb7
Yogthos <dmitri.sotnikov@gmail.com>
2015-03-22 23:23:13 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "2.1.2"
+(defproject luminus/lein-template "2.1.3"
             :description "a template for creating Luminus applications"
             :url "https://github.com/yogthos/luminus-template"
             :license {:name "MIT License"
