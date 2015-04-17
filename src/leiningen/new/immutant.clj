b48d7c363db317d7b0712e0b6a4897cfbc544fef
Yogthos <dmitri.sotnikov@gmail.com>
2015-04-17 13:30:55 -0400

    updated to the latest immutant

--- src/leiningen/new/immutant.clj
+++ src/leiningen/new/immutant.clj
@@ -5,5 +5,5 @@
   (if (some #{"+immutant"} (:features options))
     [(into (remove-conflicting-assets assets "core.clj")
            [["src/<<sanitized>>/core.clj" "immutant/core.clj"]])
-     (append-options options :dependencies [['org.immutant/web "2.0.0-beta2"]])]
+     (append-options options :dependencies [['org.immutant/web "2.0.0"]])]
     state))
