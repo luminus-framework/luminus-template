cb832d49b114978d5fbdeb21ccc2bb0c0baa0357
Yogthos <dmitri.sotnikov@gmail.com>
2015-05-03 21:51:59 -0400

    added data type coercions for sql

--- src/leiningen/new/luminus/db/src/mysql.db.clj
+++ src/leiningen/new/luminus/db/src/mysql.db.clj
@@ -1,6 +1,8 @@
 (ns <<project-ns>>.db.core
   (:require
-    [yesql.core :refer [defqueries]]))
+    [yesql.core :refer [defqueries]]
+    [clojure.java.jdbc :as jdbc])
+  (:import [java.sql PreparedStatement]))
 
 (def db-spec
   {:subprotocol "mysql"
@@ -9,3 +11,18 @@
    :password "db_user_password_here"})
 
 (defqueries "sql/queries.sql" {:connection db-spec})
+
+(defn to-date [sql-date]
+  (-> sql-date (.getTime) (java.util.Date.)))
+
+(extend-protocol jdbc/IResultSetReadColumn
+  java.sql.Date
+  (result-set-read-column [v _ _] (to-date v))
+
+  java.sql.Timestamp
+  (result-set-read-column [v _ _] (to-date v)))
+
+(extend-type java.util.Date
+  jdbc/ISQLParameter
+  (set-parameter [v ^PreparedStatement stmt idx]
+    (.setTimestamp stmt idx (java.sql.Timestamp. (.getTime v)))))
--- src/leiningen/new/luminus/db/src/postgres.db.clj
+++ src/leiningen/new/luminus/db/src/postgres.db.clj
@@ -1,6 +1,8 @@
 (ns <<project-ns>>.db.core
   (:require
-    [yesql.core :refer [defqueries]]))
+    [yesql.core :refer [defqueries]]
+    [clojure.java.jdbc :as jdbc])
+  (:import [java.sql PreparedStatement]))
 
 (def db-spec
   {:subprotocol "postgresql"
@@ -9,3 +11,24 @@
    :password "db_user_password_here"})
 
 (defqueries "sql/queries.sql" {:connection db-spec})
+
+(defn to-date [sql-date]
+  (-> sql-date (.getTime) (java.util.Date.)))
+
+(extend-protocol jdbc/IResultSetReadColumn
+  java.sql.Date
+  (result-set-read-column [v _ _] (to-date v))
+
+  java.sql.Timestamp
+  (result-set-read-column [v _ _] (to-date v))
+
+  org.postgresql.jdbc4.Jdbc4Array
+  (result-set-read-column [v _ _] (vec (.getArray v)))
+
+  org.postgresql.util.PGobject
+  (result-set-read-column [v _ _] (str v)))
+
+(extend-type java.util.Date
+  jdbc/ISQLParameter
+  (set-parameter [v ^PreparedStatement stmt idx]
+    (.setTimestamp stmt idx (java.sql.Timestamp. (.getTime v)))))
