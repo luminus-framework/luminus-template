8d2d114706f2da1b25e850b6d01d5575a3fb9d07
Yogthos <dmitri.sotnikov@gmail.com>
2015-05-16 20:21:02 -0400

    rolled back postgres json extensions

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "2.4.5"
+(defproject luminus/lein-template "2.4.6"
             :description "a template for creating Luminus applications"
             :url "https://github.com/yogthos/luminus-template"
             :license {:name "MIT License"
--- src/leiningen/new/luminus/db/src/postgres.db.clj
+++ src/leiningen/new/luminus/db/src/postgres.db.clj
@@ -1,10 +1,8 @@
 (ns <<project-ns>>.db.core
   (:require
-    [clojure.java.jdbc :as jdbc]
     [yesql.core :refer [defqueries]]
-    [cheshire.core :refer [parse-string generate-string]])
-  (:import java.sql.PreparedStatement
-           org.postgresql.util.PGobject))
+    [clojure.java.jdbc :as jdbc])
+  (:import [java.sql PreparedStatement]))
 
 (def db-spec
   {:subprotocol "postgresql"
@@ -34,24 +32,3 @@
   jdbc/ISQLParameter
   (set-parameter [v ^PreparedStatement stmt idx]
     (.setTimestamp stmt idx (java.sql.Timestamp. (.getTime v)))))
-
-(defn to-pg-json [value]
-  (doto (PGobject.)
-    (.setType "jsonb")
-    (.setValue (generate-string value))))
-
-(extend-protocol jdbc/ISQLValue
-  clojure.lang.IPersistentMap
-  (sql-value [value] (to-pg-json value))
-  clojure.lang.IPersistentVector
-  (sql-value [value] (to-pg-json value)))
-
-(extend-protocol jdbc/IResultSetReadColumn
-  PGobject
-  (result-set-read-column [pgobj _metadata _index]
-    (let [type  (.getType pgobj)
-          value (.getValue pgobj)]
-      (case type
-        "json" (parse-string value true)
-        "jsonb" (parse-string value true)
-        :else value))))
