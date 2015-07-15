837f6f37fb3d87272e59aac593d44c2b8b005b4f
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2015-07-15 18:33:37 -0400

    bumped up version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "2.8.8"
+(defproject luminus/lein-template "2.8.9"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "MIT License"
