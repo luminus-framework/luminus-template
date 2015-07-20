247f4419fc0142634cd80d850d2c36aa761a7563
Yogthos <dmitri.sotnikov@gmail.com>
2015-07-20 09:53:43 -0400

    updated to use a transaction in tests

--- src/leiningen/new/luminus/db/test/db/core.clj
+++ src/leiningen/new/luminus/db/test/db/core.clj
@@ -1,6 +1,7 @@
 (ns <<project-ns>>.test.db.core
   (:require [clojure.test :refer :all]
             [<<project-ns>>.db.core :as db]
+            [clojure.java.jdbc :as jdbc]
             [<<project-ns>>.db.migrations :as migrations]))
 
 (use-fixtures
@@ -13,20 +14,22 @@
 (deftest test-users
   ;; Make sure the user with id 1 doesn't exist.
   ;; You can also use transactions around tests to ensure that.
-  (db/execute db/delete-user! {:id "1"})
-  (is (= 1 (db/execute
-             db/create-user!
-             {:id         "1"
-              :first_name "Sam"
-              :last_name  "Smith"
-              :email      "sam.smith@example.com"
-              :pass       "pass"})))
-  (is (= [{:id         "1"
-           :first_name "Sam"
-           :last_name  "Smith"
-           :email      "sam.smith@example.com"
-           :pass       "pass"
-           :admin      nil
-           :last_login nil
-           :is_active  nil}]
-         (db/execute db/get-user {:id "1"}))))
+  (jdbc/with-db-transaction [trans-conn (:connection @db/conn)]
+    (jdbc/db-set-rollback-only! trans-conn)
+    (is (= 1 (db/execute
+               db/create-user!
+               {:id         "1"
+                :first_name "Sam"
+                :last_name  "Smith"
+                :email      "sam.smith@example.com"
+                :pass       "pass"}
+               {:connection trans-conn})))
+    (is (= [{:id         "1"
+             :first_name "Sam"
+             :last_name  "Smith"
+             :email      "sam.smith@example.com"
+             :pass       "pass"
+             :admin      nil
+             :last_login nil
+             :is_active  nil}]
+           (db/execute db/get-user {:id "1"} {:connection trans-conn})))))
