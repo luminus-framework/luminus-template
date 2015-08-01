7e342f8b3543d40f6ee976533692470a095e490b
Yogthos <dmitri.sotnikov@gmail.com>
2015-08-01 13:50:52 -0400

    fixed session store definition

--- src/leiningen/new/luminus/core/src/session.clj
+++ src/leiningen/new/luminus/core/src/session.clj
@@ -1,6 +1,4 @@
 (ns <<project-ns>>.session
   (:require [ring-ttl-session.core :refer [ttl-memory-store]]))
 
-(defonce ttl-mem 
-  "Session storage whose keys expire in 30 minutes."
-  (ttl-memory-store (* 60 30)))
+(defonce ttl-mem (ttl-memory-store (* 60 30)))
