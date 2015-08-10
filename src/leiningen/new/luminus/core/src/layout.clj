2090a5c76f5fb8e0f6981ded869c047bdfa5a4fe
Yogthos <dmitri.sotnikov@gmail.com>
2015-08-10 08:52:05 -0400

    fixed error-page docs

--- src/leiningen/new/luminus/core/src/layout.clj
+++ src/leiningen/new/luminus/core/src/layout.clj
@@ -33,7 +33,8 @@
    :title - error title (optional)
    :message - detailed error message (optional)
 
-   returns HTML string"
+   returns a response map with the error page as the body
+   and the status specified by the status key"
   [error-details]
   {:status  (:status error-details)
    :headers {"Content-Type" "text/html; charset=utf-8"}
