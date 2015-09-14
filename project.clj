a41d1b0ed0a629c3bd01b6fcd89830da6c651fa9
Yogthos <dmitri.sotnikov@gmail.com>
2015-09-14 00:23:26 -0400

    updated to only show cljs compilation notes in dev mode

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "2.9.8.37"
+(defproject luminus/lein-template "2.9.8.38"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "MIT License"
