7eae37475e09091edf91412535a19ed843e6f146
Yogthos <dmitri.sotnikov@gmail.com>
2015-12-31 10:55:14 -0500

    bumped up selmer

--- src/leiningen/new/luminus/core/project.clj
+++ src/leiningen/new/luminus/core/project.clj
@@ -4,7 +4,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [selmer "0.9.7"]
+                 [selmer "0.9.8"]
                  [markdown-clj "0.9.85"]
                  [environ "1.0.1"]
                  [ring-middleware-format "0.7.0"]
