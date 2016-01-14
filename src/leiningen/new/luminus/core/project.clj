08805a032067e3e79a7441901480330e8b4325f3
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-13 23:10:36 -0500

    bumped up version

--- src/leiningen/new/luminus/core/project.clj
+++ src/leiningen/new/luminus/core/project.clj
@@ -4,7 +4,7 @@
   :url "http://example.com/FIXME"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [selmer "0.9.9"]
+                 [selmer "1.0.0"]
                  [markdown-clj "0.9.85"]
                  [environ "1.0.1"]
                  [ring-middleware-format "0.7.0"]
