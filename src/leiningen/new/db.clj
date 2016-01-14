f01e3a2a752269c7dfb71381517001b22d69310e
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-01-14 18:19:07 -0500

    Update db.clj

--- src/leiningen/new/db.clj
+++ src/leiningen/new/db.clj
@@ -13,7 +13,7 @@
   [['migratus "0.8.8"]
    ['conman "0.2.8"]
    ({:postgres ['org.postgresql/postgresql "9.4-1206-jdbc4"]
-     :mysql    ['mysql/mysql-connector-java "5.1.34"]
+     :mysql    ['mysql/mysql-connector-java "5.1.6"]
      :h2       ['com.h2database/h2 "1.4.190"]
      :sqlite   ['org.xerial/sqlite-jdbc "3.8.11.1"]}
      (select-db options))])
