85b45c7b343a2e2e0eb6defa86c74985321f96e1
Yogthos <dmitri.sotnikov@gmail.com>
2016-01-14 12:38:57 -0500

    bumped up dependencies

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject luminus/lein-template "2.9.8.88"
+(defproject luminus/lein-template "2.9.8.89"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :eval-in-leiningen true
-  :dependencies [[leinjacker "0.2.0"]
-                 [selmer "0.9.0"]])
+  :dependencies [[leinjacker "0.4.2"]
+                 [selmer "1.0.0"]])
