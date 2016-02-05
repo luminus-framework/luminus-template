930b2a14d6f5e205b0c3123365f979a89d98af4f
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-05 09:29:17 -0500

    reindented swagger routes

--- src/leiningen/new/luminus/swagger/src/services.clj
+++ src/leiningen/new/luminus/swagger/src/services.clj
@@ -16,58 +16,58 @@
                            :title "Sample API"
                            :description "Sample Services"}}}}
   (context "/api" []
-           :tags ["thingie"]
+    :tags ["thingie"]
 
-           (GET "/plus" []
-                :return       Long
-                :query-params [x :- Long, {y :- Long 1}]
-                :summary      "x+y with query-parameters. y defaults to 1."
-                (ok (+ x y)))
+    (GET "/plus" []
+      :return       Long
+      :query-params [x :- Long, {y :- Long 1}]
+      :summary      "x+y with query-parameters. y defaults to 1."
+      (ok (+ x y)))
 
-           (POST "/minus" []
-                 :return      Long
-                 :body-params [x :- Long, y :- Long]
-                 :summary     "x-y with body-parameters."
-                 (ok (- x y)))
+    (POST "/minus" []
+      :return      Long
+      :body-params [x :- Long, y :- Long]
+      :summary     "x-y with body-parameters."
+      (ok (- x y)))
 
-           (GET "/times/:x/:y" []
-                :return      Long
-                :path-params [x :- Long, y :- Long]
-                :summary     "x*y with path-parameters"
-                (ok (* x y)))
+    (GET "/times/:x/:y" []
+      :return      Long
+      :path-params [x :- Long, y :- Long]
+      :summary     "x*y with path-parameters"
+      (ok (* x y)))
 
-           (POST "/divide" []
-                 :return      Double
-                 :form-params [x :- Long, y :- Long]
-                 :summary     "x/y with form-parameters"
-                 (ok (/ x y)))
+    (POST "/divide" []
+      :return      Double
+      :form-params [x :- Long, y :- Long]
+      :summary     "x/y with form-parameters"
+      (ok (/ x y)))
 
-           (GET "/power" []
-                :return      Long
-                :header-params [x :- Long, y :- Long]
-                :summary     "x^y with header-parameters"
-                (ok (long (Math/pow x y))))
+    (GET "/power" []
+      :return      Long
+      :header-params [x :- Long, y :- Long]
+      :summary     "x^y with header-parameters"
+      (ok (long (Math/pow x y))))
 
-           (PUT "/echo" []
-                :return   [{:hot Boolean}]
-                :body     [body [{:hot Boolean}]]
-                :summary  "echoes a vector of anonymous hotties"
-                (ok body))
+    (PUT "/echo" []
+      :return   [{:hot Boolean}]
+      :body     [body [{:hot Boolean}]]
+      :summary  "echoes a vector of anonymous hotties"
+      (ok body))
 
-           (POST "/echo" []
-                 :return   (s/maybe Thingie)
-                 :body     [thingie (s/maybe Thingie)]
-                 :summary  "echoes a Thingie from json-body"
-                 (ok thingie)))
+    (POST "/echo" []
+      :return   (s/maybe Thingie)
+      :body     [thingie (s/maybe Thingie)]
+      :summary  "echoes a Thingie from json-body"
+      (ok thingie)))
 
   (context "/context" []
-           :tags ["context"]
-           :summary "summary inherited from context"
-           (context "/:kikka" []
-                    :path-params [kikka :- s/Str]
-                    :query-params [kukka :- s/Str]
-                    (GET "/:kakka" []
-                         :path-params [kakka :- s/Str]
-                         (ok {:kikka kikka
-                              :kukka kukka
-                              :kakka kakka})))))
+    :tags ["context"]
+    :summary "summary inherited from context"
+    (context "/:kikka" []
+      :path-params [kikka :- s/Str]
+      :query-params [kukka :- s/Str]
+      (GET "/:kakka" []
+        :path-params [kakka :- s/Str]
+        (ok {:kikka kikka
+             :kukka kukka
+             :kakka kakka})))))
