701068b9542bb0870b44e575b941c65128257844
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-13 18:15:57 -0500

    cleanup for home route requires

--- src/leiningen/new/luminus/core/src/home.clj
+++ src/leiningen/new/luminus/core/src/home.clj
@@ -1,7 +1,7 @@
 (ns <<project-ns>>.routes.home
   (:require [<<project-ns>>.layout :as layout]
             [compojure.core :refer [defroutes GET]]
-            [ring.util.http-response :refer [ok]]
+            [ring.util.http-response :as response]
             [clojure.java.io :as io]))
 <% if  cljs  %>
 (defn home-page []
