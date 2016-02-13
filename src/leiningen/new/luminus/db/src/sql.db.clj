93a98a4035447cc9bdc0df6620d04b264fdcf940
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-02-12 21:36:51 -0500

    added postgres array serialization

--- src/leiningen/new/luminus/db/src/sql.db.clj
+++ src/leiningen/new/luminus/db/src/sql.db.clj
@@ -121,6 +121,16 @@
     (.setType "jsonb")
     (.setValue (generate-string value))))
 
+(extend-protocol jdbc/ISQLParameter
+  clojure.lang.IPersistentVector
+  (set-parameter [v ^java.sql.PreparedStatement stmt ^long i]
+    (let [conn      (.getConnection stmt)
+          meta      (.getParameterMetaData stmt)
+          type-name (.getParameterTypeName meta i)]
+      (if-let [elem-type (when (= (first type-name) \_) (apply str (rest type-name)))]
+        (.setObject stmt i (.createArrayOf conn elem-type (to-array v)))
+        (.setObject stmt i v)))))
+      
 (extend-protocol jdbc/ISQLValue
   IPersistentMap
   (sql-value [value] (to-pg-json value))
