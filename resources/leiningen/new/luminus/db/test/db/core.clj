83b8e56c4c9a770cf566b0f183b7d241cdc59383
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-21 20:04:42 -0500

    fixed generated tests for db profiles

--- resources/leiningen/new/luminus/db/test/db/core.clj
+++ resources/leiningen/new/luminus/db/test/db/core.clj
@@ -1,28 +1,28 @@
 (ns <<project-ns>>.test.db.core
-  (:require [<<project-ns>>.db.core :as db]
+  (:require [<<project-ns>>.db.core :refer [*db*] :as db]
             [<<project-ns>>.db.migrations :as migrations]
             [clojure.test :refer :all]
-            [clojure.java.jdbc :as jdbc]<% if not embedded-db %>
-            [conman.core :refer [with-transaction]]<% endif %>
+            [clojure.java.jdbc :as jdbc]
             [config.core :refer [env]]
             [mount.core :as mount]))
 
 (use-fixtures
   :once
-  (fn [f]<% ifunequal db-type "h2" %>
-    (mount/start #'<<project-ns>>.db.core/*db*)<% endifunequal %>
+  (fn [f]
+    (mount/start #'<<project-ns>>.db.core/*db*)
     (migrations/migrate ["migrate"])
     (f)))
 
 (deftest test-users
-  (<% if embedded-db %>jdbc/with-db-transaction [t-conn db/conn]<% else %>with-transaction [t-conn db/*db*]<% endif %>
+  (jdbc/with-db-transaction [t-conn *db*]
     (jdbc/db-set-rollback-only! t-conn)
     (is (= 1 (db/create-user!
+               t-conn
                {:id         "1"
                 :first_name "Sam"
                 :last_name  "Smith"
                 :email      "sam.smith@example.com"
-                :pass       "pass"}<% ifequal db-type "h2" %> {:connection t-conn}<% endifequal %>)))
+                :pass       "pass"})))
     (is (= {:id         "1"
             :first_name "Sam"
             :last_name  "Smith"
@@ -31,4 +31,4 @@
             :admin      nil
             :last_login nil
             :is_active  nil}
-           (db/get-user {:id "1"}<% ifequal db-type "h2" %> {:connection t-conn}<% endifequal %>)))))
+           (db/get-user t-conn {:id "1"})))))
