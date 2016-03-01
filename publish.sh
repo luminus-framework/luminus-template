8bd94b2af5017c7d1f101c9013d1952a0df7500d
Yogthos <dmitri.sotnikov@gmail.com>
2016-02-29 20:26:30 -0500

    relese "6.01.9.2"

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
+README="resources/leiningen/new/luminus/core/README.md"
 VERSION=`head -1 project.clj | rev | cut -d" " -f1`
-sed "s/<luminus-version>/$VERSION/g" resources/leiningen/new/luminus/core/README.md
-echo published Luminus $VERSION
+sed "s/<luminus-version>/$VERSION/g" "$README".in > $README
 git commit -a -m "relese $VERSION" && git push
 echo published Luminus $VERSION
