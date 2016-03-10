c5cde68ae4f15de071d63c77e2e38fab3b1e6f20
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-03-10 15:18:36 -0500

    removed explicit ring dependency

--- resources/leiningen/new/luminus/core/project.clj
+++ resources/leiningen/new/luminus/core/project.clj
@@ -18,7 +18,6 @@
                  [ring-webjars "0.1.1"]
                  [ring/ring-defaults "0.2.0"]<% if not immutant-session %>
                  [ring-ttl-session "0.3.0"]<% endif %>
-                 [ring "1.4.0" :exclusions [ring/ring-jetty-adapter]]
                  [mount "0.1.10"]
                  [cprop "0.1.6"]
                  [org.clojure/tools.cli "0.3.3"]
