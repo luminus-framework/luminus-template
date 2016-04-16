0d33a97af247616c93f10d5d2adc16b5e5dacc31
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-04-15 23:07:46 -0400

    bumped up buddy

--- src/leiningen/new/auth.clj
+++ src/leiningen/new/auth.clj
@@ -5,7 +5,7 @@
   (if (some #{"+auth"} (:features options))
     [assets
      (-> options
-         (append-options :dependencies [['buddy "0.11.0"]])
+         (append-options :dependencies [['buddy "0.12.0"]])
          (append-formatted :auth-middleware-required
                            [['buddy.auth.middleware :refer ['wrap-authentication 'wrap-authorization]]
                             ['buddy.auth.backends.session :refer ['session-backend]]
