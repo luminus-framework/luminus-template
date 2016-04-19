799b4f7521ef12d6fa21e6c670b556db63ed9082
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-04-19 16:29:41 -0400

    cleaned up service API example

--- resources/leiningen/new/luminus/swagger/src/services.clj
+++ resources/leiningen/new/luminus/swagger/src/services.clj
@@ -3,12 +3,6 @@
             [compojure.api.sweet :refer :all]
             [schema.core :as s]))
 
-(s/defschema Thingie {:id Long
-                      :hot Boolean
-                      :tag (s/enum :kikka :kukka)
-                      :chief [{:name String
-                               :type #{{:id String}}}]})
-
 (defapi service-routes
   {:swagger {:ui "/swagger-ui"
              :spec "/swagger.json"
@@ -46,28 +40,4 @@
       :return      Long
       :header-params [x :- Long, y :- Long]
       :summary     "x^y with header-parameters"
-      (ok (long (Math/pow x y))))
-
-    (PUT "/echo" []
-      :return   [{:hot Boolean}]
-      :body     [body [{:hot Boolean}]]
-      :summary  "echoes a vector of anonymous hotties"
-      (ok body))
-
-    (POST "/echo" []
-      :return   (s/maybe Thingie)
-      :body     [thingie (s/maybe Thingie)]
-      :summary  "echoes a Thingie from json-body"
-      (ok thingie)))
-
-  (context "/context" []
-    :tags ["context"]
-    :summary "summary inherited from context"
-    (context "/:kikka" []
-      :path-params [kikka :- s/Str]
-      :query-params [kukka :- s/Str]
-      (GET "/:kakka" []
-        :path-params [kakka :- s/Str]
-        (ok {:kikka kikka
-             :kukka kukka
-             :kakka kakka})))))
+      (ok (long (Math/pow x y))))))
