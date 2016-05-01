40483bf2a430eb15de3f812db4ed4d3d9d38da99
Yogthos <dmitri.sotnikov@gmail.com>
2016-04-30 23:46:51 -0400

    release "2.9.10.46"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "2.9.10.45"
+(defproject luminus/lein-template "2.9.10.46"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "MIT License"
--- resources/leiningen/new/luminus/core/README.md
+++ resources/leiningen/new/luminus/core/README.md
@@ -1,6 +1,6 @@
 # <<name>>
 
-generated using Luminus version "2.9.10.45"
+generated using Luminus version "2.9.10.46"
 
 FIXME
 
