1e9118939deaf6190428b4b78398f7d63ee809f9
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-05-21 14:00:31 -0400

    fixed tests for war profile

--- resources/leiningen/new/luminus/core/test/handler.clj
+++ resources/leiningen/new/luminus/core/test/handler.clj
@@ -5,9 +5,9 @@
 
 (deftest test-app
   (testing "main route"
-    (let [response ((app) (request :get "/"))]
+    (let [response (<% if war %>app<% else %>(app)<% endif %> (request :get "/"))]
       (is (= 200 (:status response)))))
 
   (testing "not-found route"
-    (let [response ((app) (request :get "/invalid"))]
+    (let [response (<% if war %>app<% else %>(app)<% endif %> (request :get "/invalid"))]
       (is (= 404 (:status response))))))
