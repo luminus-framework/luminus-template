fa95d37e4707e7fd791cbc4189d18f8d445f921d
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-06-07 12:00:02 -0400

    added lein-immutant as the default plugin

--- src/leiningen/new/immutant.clj
+++ src/leiningen/new/immutant.clj
@@ -1,6 +1,9 @@
 (ns leiningen.new.immutant
   (:require [leiningen.new.common :refer :all]))
 
+(def immutant-plugins
+  [['lein-immutant "2.1.0"]])
+
 (defn immutant-features [[assets options :as state]]
   (if (some #{"+immutant"} (:features options))
     [assets
@@ -8,6 +11,7 @@
          (assoc
            :immutant-session true
            :server "immutant")
+         (append-options :plugins immutant-plugins)
          (update :dependencies #(remove #{['luminus/ring-ttl-session "0.3.1"]} %))
          (append-options :http-server-dependencies
                          [['org.webjars/webjars-locator-jboss-vfs "0.1.0"]
