ea2b2167479bc343263197f544eca65f9a279dc8
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-09-25 19:56:11 -0400

    fixed formatting

--- resources/leiningen/new/luminus/reframe/src/cljs/core.cljs
+++ resources/leiningen/new/luminus/reframe/src/cljs/core.cljs
@@ -12,11 +12,12 @@
   (:import goog.History))
 
 (defn nav-link [uri title page collapsed?]
-  (let [selected-page (rf/subscribe [:page])] [:li.nav-item
-   {:class (when (= page @selected-page) "active")}
-   [:a.nav-link
-    {:href uri
-     :on-click #(reset! collapsed? true)} title]]))
+  (let [selected-page (rf/subscribe [:page])]
+    [:li.nav-item
+     {:class (when (= page @selected-page) "active")}
+     [:a.nav-link
+      {:href uri
+       :on-click #(reset! collapsed? true)} title]]))
 
 (defn navbar []
   (r/with-let [collapsed? (r/atom true)]
