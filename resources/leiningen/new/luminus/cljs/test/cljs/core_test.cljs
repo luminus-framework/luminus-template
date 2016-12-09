7917391633f06212342539022f1ff28a07dda67c
Dexter Gramfors <dexter@kth.se>
2016-12-09 02:07:49 +0100

    Make humane-test-output work in ClojureScript (https://github.com/pjstadig/humane-test-output/issues/15)

--- resources/leiningen/new/luminus/cljs/test/cljs/core_test.cljs
+++ resources/leiningen/new/luminus/cljs/test/cljs/core_test.cljs
@@ -1,5 +1,6 @@
 (ns <<project-ns>>.core-test
   (:require [cljs.test :refer-macros [is are deftest testing use-fixtures]]
+            [pjstadig.humane-test-output]
             [reagent.core :as reagent :refer [atom]]
             [<<project-ns>>.core :as rc]))
 
