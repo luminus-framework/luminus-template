df2266b7d8b272c6d23405e57e59ed52f292591a
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-12-09 12:31:29 -0500

    excluded http server from start/stop cycle

--- resources/leiningen/new/luminus/core/env/dev/clj/user.clj
+++ resources/leiningen/new/luminus/core/env/dev/clj/user.clj
@@ -4,10 +4,12 @@
             <<project-ns>>.core))
 
 (defn start []
-  (mount/start-without #'<<project-ns>>.core/repl-server))
+  (mount/start-without #'<<project-ns>>.core/http-server
+                       #'<<project-ns>>.core/repl-server))
 
 (defn stop []
-  (mount/stop-except #'<<project-ns>>.core/repl-server))
+  (mount/stop-except #'<<project-ns>>.core/http-server
+                     #'<<project-ns>>.core/repl-server))
 
 (defn restart []
   (stop)
