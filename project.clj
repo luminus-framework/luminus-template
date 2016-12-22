67353f9daa429584f0f84d9b04c64a6560182170
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2016-12-22 11:46:24 -0500

    bumped up selmer

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "2.9.11.18"
+(defproject luminus/lein-template "2.9.11.19"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "MIT License"
