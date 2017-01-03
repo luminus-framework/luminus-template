a67d46c8b345c71a4c9fe2e006f2f83ef529fb3d
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-01-03 17:04:56 -0500

    fixed typos in hoplon profile

--- src/leiningen/new/hoplon.clj
+++ src/leiningen/new/hoplon.clj
@@ -1,11 +1,11 @@
 (ns leiningen.new.hoplon
   (:require [leiningen.new.common :refer :all]))
 
-(def reagent-assets
+(def hoplon-assets
   [["src/cljs/{{sanitized}}/core.cljs" "hoplon/src/cljs/core.cljs"]
    ["src/cljs/{{sanitized}}/ajax.cljs" "hoplon/src/cljs/ajax.cljs"]])
 
-(def reagent-dependencies
+(def hoplon-dependencies
   [['hoplon "6.0.0-alpha17"]
    ['cljsjs/jquery "2.2.4-0"]
    ['secretary "1.2.3"]
@@ -13,8 +13,8 @@
 
 (defn hoplon-features [[assets options :as state]]
   (if (some #{"+hoplon"} (:features options))
-    [(into (remove-conflicting-assets assets "core.cljs") reagent-assets)
+    [(into (remove-conflicting-assets assets "core.cljs") hoplon-assets)
      (-> options
          (assoc :hoplon true)
-         (append-options :dependencies reagent-dependencies))]
+         (append-options :dependencies hoplon-dependencies))]
     state))
