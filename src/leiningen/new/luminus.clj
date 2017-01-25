38882cd58c36e5d792cb350c86b048e2b6c5c79f
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2017-01-25 12:08:58 -0500

    bumped up markdown-clj

--- src/leiningen/new/luminus.clj
+++ src/leiningen/new/luminus.clj
@@ -96,7 +96,7 @@
 (def core-dependencies
   [['org.clojure/clojure "1.8.0"]
    ['selmer "1.10.5"]
-   ['markdown-clj "0.9.91"]
+   ['markdown-clj "0.9.92"]
    ['ring-middleware-format "0.7.0"]
    ['metosin/ring-http-response "0.8.1"]
    ['bouncer "1.0.0"]
