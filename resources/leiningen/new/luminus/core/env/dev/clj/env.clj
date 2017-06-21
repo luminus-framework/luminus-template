ed2629c901abe3e0ca73a420bcf67ca8d2a2e5f0
Sotnikov, Dmitri <dmitri.sotnikov@gmail.com>
2017-06-21 18:15:44 -0400

    moved profiles config to dev env

--- resources/leiningen/new/luminus/core/env/dev/clj/env.clj
+++ resources/leiningen/new/luminus/core/env/dev/clj/env.clj
@@ -1,7 +1,21 @@
 (ns <<project-ns>>.env
-  (:require [selmer.parser :as parser]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
             [clojure.tools.logging :as log]
+            [selmer.parser :as parser]
             [<<project-ns>>.dev-middleware :refer [wrap-dev]]))
+
+(defn read-profile [profiles-file profile]
+  (when (.exists (io/file profiles-file))
+    (-> profiles-file slurp edn/read-string profile :env)))
+
+(defn from-profiles []
+  (let [profiles-file "profiles.clj"
+        env (-> "config.edn" io/resource slurp edn/read-string :env)]
+    (case env
+      :dev (read-profile profiles-file :profiles/dev)
+      :test (read-profile profiles-file :profiles/test)
+      {})))
 
 (def defaults
   {:init
--- resources/leiningen/new/luminus/core/src/config.clj
+++ resources/leiningen/new/luminus/core/src/config.clj
@@ -1,21 +1,8 @@
 (ns <<project-ns>>.config
-  (:require [clojure.edn :as edn]
-            [clojure.java.io :as io]
+  (:require [<<project-ns>>.env :refer [from-profiles]]
             [cprop.core :refer [load-config]]
             [cprop.source :as source]
             [mount.core :refer [args defstate]]))
-
-(defn read-profile [profiles-file profile]
-  (when (.exists (io/file profiles-file))
-    (-> profiles-file slurp edn/read-string profile :env)))
-
-(defn from-profiles []
-  (let [profiles-file "profiles.clj"
-        env (-> "config.edn" io/resource slurp edn/read-string :env)]
-    (case env
-      :dev (read-profile profiles-file :profiles/dev)
-      :test (read-profile profiles-file :profiles/test)
-      {})))
 
 (defstate env :start (load-config
                        :merge
