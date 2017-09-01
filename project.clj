008d58f11076209cdac77fef84a885d1c9ce0cd9
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-09-01 10:08:26 -0400

    release "2.9.11.87"

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject luminus/lein-template "2.9.11.86"
+(defproject luminus/lein-template "2.9.11.87"
   :description "a template for creating Luminus applications"
   :url "https://github.com/yogthos/luminus-template"
   :license {:name "MIT License"
--- resources/leiningen/new/luminus/core/README.md
+++ resources/leiningen/new/luminus/core/README.md
@@ -1,6 +1,6 @@
 # <<name>>
 
-generated using Luminus version "2.9.11.86"
+generated using Luminus version "2.9.11.87"
 
 FIXME
 
