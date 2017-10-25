e25602ecf5af4740c7c1ae661f6432ca3b4b430f
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2017-10-25 19:22:36 -0400

    release "2.9.11.98"

--- resources/leiningen/new/luminus/core/src/home.clj
+++ resources/leiningen/new/luminus/core/src/home.clj
@@ -3,14 +3,13 @@
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
             [clojure.java.io :as io]))
-<% if  cljs  %>
+<% if cljs  %>
 (defn home-page []
   (layout/render "home.html"))
 
 (defroutes home-routes
   (GET "/" []
-       (home-page))<% if  graphql  %>
-  (GET "/graphiql" [] (layout/render "graphiql.html"))<% endif %>
+       (home-page))
   (GET "/docs" []
        (-> (response/ok (-> "docs/docs.md" io/resource slurp))
            (response/header "Content-Type" "text/plain; charset=utf-8"))))
@@ -23,6 +22,7 @@
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" [] (home-page))<% if graphql %>
+  (GET "/graphiql" [] (layout/render "graphiql.html"))<% endif %>
   (GET "/about" [] (about-page)))
 <% endif %>
