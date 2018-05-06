f6390d5fb8fa90017aab2af160d50852c549b7d1
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2018-05-06 12:07:45 -0400

    added graphql, cljs routes for reitit

--- resources/leiningen/new/luminus/reitit/src/home.clj
+++ resources/leiningen/new/luminus/reitit/src/home.clj
@@ -1,8 +1,21 @@
 (ns <<project-ns>>.routes.home
   (:require [<<project-ns>>.layout :as layout]
             [clojure.java.io :as io]
-            [myapp.middleware :as middleware]))
+            [myapp.middleware :as middleware]
+            [ring.util.http-response :as response]))
+<% if cljs  %>
+(defn home-page [_]
+  (layout/render "home.html"))
 
+(defn home-routes []
+  [["/" {:get {:handler    home-page
+               :middleware [middleware/wrap-csrf
+                            middleware/wrap-formats]}}]<% if graphql %>
+   ["/graphiql" {:get {:handler (fn [_] (layout/render "graphiql.html"))}}]<% endif %>
+   ["/docs" {:get {:handler (fn [_]
+                              (-> (response/ok (-> "docs/docs.md" io/resource slurp))
+                                  (response/header "Content-Type" "text/plain; charset=utf-8")))}}]])
+<% else %>
 (defn home-page [_]
   (layout/render
     "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
@@ -13,5 +26,8 @@
 (defn home-routes []
   [["/" {:get {:handler    home-page
                :middleware [middleware/wrap-csrf
-                            middleware/wrap-formats]}}]
+                            middleware/wrap-formats]}}]<% if graphql %>
+   ["/graphiql" {:get {:handler (fn [_] (layout/render "graphiql.html"))}}]
+   <% endif %>
    ["/about" {:get about-page}]])
+<% endif %>
