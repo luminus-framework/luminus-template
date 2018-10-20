8640b2c6b8d1e16f39a1803e5071098fc4a4522b
ingesolvoll <inge.solvoll@gmail.com>
2018-10-20 21:03:08 +0200

    Removing route-by-name event, will fix by supporting 2 arities in reitit/match-by-name

--- resources/leiningen/new/luminus/keeframe/src/cljs/routing.cljs
+++ resources/leiningen/new/luminus/keeframe/src/cljs/routing.cljs
@@ -1,8 +1,6 @@
 (ns <<project-ns>>.routing
   (:require
-    [re-frame.core :as rf]
-    [reitit.core :as reitit])
-  (:import goog.History))
+    [re-frame.core :as rf]))
 
 (def routes
   [["/" :home]
@@ -12,13 +10,6 @@
   :nav/route
   :<- [:kee-frame/route]
   identity)
-
-(rf/reg-event-fx
-  :nav/by-route-name
-  (fn [_ [_ route-name]]
-    (let [route (reitit/match-by-name router route-name)]
-      {:navigate-to [(-> route :data :name) (:path-params route)]})))
-
 
 (rf/reg-sub
   :nav/page
