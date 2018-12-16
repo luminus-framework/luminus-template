5ab9049c605b8b9eab0256e10a86a560d6120634
Tommi Reiman <tommi@iki.fi>
2018-12-16 19:16:47 +0200

    Add sample tests for swagger

--- resources/leiningen/new/luminus/reitit/src/services.clj
+++ resources/leiningen/new/luminus/reitit/src/services.clj
@@ -58,13 +58,13 @@
     ["/plus"
      {:get {:summary "plus with spec query parameters"
             :parameters {:query {:x int?, :y int?}}
-            :responses {200 {:body {:total int?}}}
+            :responses {200 {:body {:total pos-int?}}}
             :handler (fn [{{{:keys [x y]} :query} :parameters}]
                        {:status 200
                         :body {:total (+ x y)}})}
       :post {:summary "plus with spec body parameters"
              :parameters {:body {:x int?, :y int?}}
-             :responses {200 {:body {:total int?}}}
+             :responses {200 {:body {:total pos-int?}}}
              :handler (fn [{{{:keys [x y]} :body} :parameters}]
                         {:status 200
                          :body {:total (+ x y)}})}}]]
