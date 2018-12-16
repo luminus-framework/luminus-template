0345a6cb512ce1a4138e4b10a7c5cb07a6ad669e
Tommi Reiman <tommi@iki.fi>
2018-12-16 19:24:04 +0200

    Service tests (#409)
    * Remove extra noice: Time-module is registered already
    
    * Add sample tests for swagger

--- resources/leiningen/new/luminus/core/src/formats.clj
+++ resources/leiningen/new/luminus/core/src/formats.clj
@@ -1,16 +1,11 @@
 (ns <<project-ns>>.middleware.formats
   (:require [cognitect.transit :as transit]
             [luminus-transit.time :as time]
-            [muuntaja.core :as m])
-  (:import [com.fasterxml.jackson.datatype.jdk8 Jdk8Module]
-           [java.time LocalDateTime]))
+            [muuntaja.core :as m]))
 
 (def instance
   (m/create
     (-> m/default-options
-        (assoc-in
-          [:formats "application/json" :opts :modules]
-          [(Jdk8Module.)])
         (update-in
           [:formats "application/transit+json" :decoder-opts]
           (partial merge time/time-deserialization-handlers))
--- resources/leiningen/new/luminus/core/test/handler.clj
+++ resources/leiningen/new/luminus/core/test/handler.clj
@@ -23,4 +23,30 @@
 
   (testing "not-found route"
     (let [response (app (request :get "/invalid"))]
-      (is (= 404 (:status response))))))
+      (is (= 404 (:status response)))))<% if swagger %>
+
+  (testing "services"
+
+    (testing "success"
+      (let [response (app (-> (request :post "/api/math/plus")
+                              (json-body {:x 10, :y 6})))]
+        (is (= 200 (:status response)))
+        (is (= {:total 16} (m/decode-response-body response)))))
+
+    (testing "parameter coercion error"
+      (let [response (app (-> (request :post "/api/math/plus")
+                              (json-body {:x 10, :y "invalid"})))]
+        (is (= 400 (:status response)))))
+
+    (testing "response coercion error"
+      (let [response (app (-> (request :post "/api/math/plus")
+                              (json-body {:x -10, :y 6})))]
+        (is (= 500 (:status response)))))
+
+    (testing "content negotiation"
+      (let [response (app (-> (request :post "/api/math/plus")
+                              (body (pr-str {:x 10, :y 6}))
+                              (content-type "application/edn")
+                              (header "accept" "application/transit+json")))]
+        (is (= 200 (:status response)))
+        (is (= {:total 16} (m/decode-response-body response))))))<% endif %>)
