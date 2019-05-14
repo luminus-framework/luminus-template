77bcb9a3cb5318de46539292f490fe824da9f4e2
defcool <defcool@163.com>
2019-05-14 22:58:22 +0800

    fix bug about emacs-cider.

--- src/leiningen/new/emacs_cider.clj
+++ src/leiningen/new/emacs_cider.clj
@@ -6,12 +6,13 @@
   (some #{"+emacs-cider"} features))
 
 (defn emacs-cider-features [state]
-  (when (has-select? (second state))
+  (if (has-select? (second state))
     [(first state)
      (-> (second state)
          (assoc :emacs-cider true)
          (append-options :dev-dependencies [['refactor-nrepl "2.4.0"]
                                         ['cider/cider-nrepl "0.21.1"]
                                         ])
-         )])
+         )]
+    state)
   )
