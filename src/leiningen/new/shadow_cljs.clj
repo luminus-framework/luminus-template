74faa20d8e5929cfc4ffb415838bf6907a3d832e
Nikola Peric <nik@nikperic.com>
2019-05-22 22:18:36 -0400

    Bump lein-shadow to 0.1.3

--- src/leiningen/new/shadow_cljs.clj
+++ src/leiningen/new/shadow_cljs.clj
@@ -5,7 +5,7 @@
                                 [org.clojure/google-closure-library "0.0-20190213-2033d5d9":scope "provided"]
                                 [thheller/shadow-cljs "2.8.37":scope "provided"]])
 
-(def shadow-cljs-plugins [['lein-shadow "0.1.2"]])
+(def shadow-cljs-plugins [['lein-shadow "0.1.3"]])
 
 (defn project-ns-symbol [project-ns suffix]
   (read-string (str project-ns suffix)))
