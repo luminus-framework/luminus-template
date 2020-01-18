319a5e7801d73c331ed17ca4d0c091856d912b96
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2020-01-18 18:50:42 -0500

    use reitit for history things when +re-frame is specified (#464)
    Co-authored-by: Carmen La <carmen.wla@gmail.com>

--- resources/leiningen/new/luminus/reframe/src/cljs/events.cljs
+++ resources/leiningen/new/luminus/reframe/src/cljs/events.cljs
@@ -1,14 +1,31 @@
 (ns <<project-ns>>.events
   (:require
     [re-frame.core :as rf]
-    [ajax.core :as ajax]))
+    [ajax.core :as ajax]
+    <% if reitit %>
+    [reitit.frontend.easy :as rfe]
+    [reitit.frontend.controllers :as rfc]
+    <% endif %>))
 
 ;;dispatchers
 <% if reitit %>
 (rf/reg-event-db
   :navigate
-  (fn [db [_ route]]
-    (assoc db :route route)))
+  (fn [db [_ match]]
+    (let [old-match (:common/route db)
+          new-match (assoc match :controllers
+                                 (rfc/apply-controllers (:controllers old-match) match))]
+      (assoc db :route new-match))))
+
+(rf/reg-fx
+  :navigate-fx!
+  (fn [[k & [params query]]]
+    (rfe/push-state k params query)))
+
+(rf/reg-event-fx
+  :navigate!
+  (fn [_ [_ url-key params query]]
+    {:navigate-fx! [url-key params query]}))
 <% else %>
 (rf/reg-event-db
   :navigate
@@ -33,6 +50,13 @@
   (fn [db [_ error]]
     (assoc db :common/error error)))
 
+<% if reitit %>
+(rf/reg-event-fx
+  :page/init-home
+  (fn [_ _]
+    {:dispatch [:fetch-docs]}))
+<% endif %>
+
 ;;subscriptions
 <% if reitit %>
 (rf/reg-sub
@@ -41,10 +65,16 @@
     (-> db :route)))
 
 (rf/reg-sub
-  :page
+  :page-id
   :<- [:route]
   (fn [route _]
     (-> route :data :name)))
+
+(rf/reg-sub
+  :page
+  :<- [:route]
+  (fn [route _]
+    (-> route :data :view)))
 <% else %>
 (rf/reg-sub
   :page
