b6c5688f1e81d7c2b53d70dc04d25746dc684169
Andrew Suzuki <andrew.b.suzuki@gmail.com>
2020-03-29 10:28:36 -0400

    Update db protocols for next.jdbc (#477)
    * Update db protocols for next.jdbc
    
    * remove end-of-file newline from postgres-fragment
    
    * oops, lost endifequal
    
    * db.core format and remove java-time requires
    
    * db.core remove java.sql imports

--- resources/leiningen/new/luminus/db/src/sql.db.clj
+++ resources/leiningen/new/luminus/db/src/sql.db.clj
@@ -1,41 +1,33 @@
 (ns <<project-ns>>.db.core<% if embedded-db %>
   (:require
-    [clojure.java.jdbc :as jdbc]
+    [next.jdbc.date-time]
+    [next.jdbc.result-set]
     [conman.core :as conman]
-    [java-time.pre-java8 :as jt]
     [mount.core :refer [defstate]]
     [<<project-ns>>.config :refer [env]])<% endif %><% ifequal db-type "postgres" %>
   (:require
     [cheshire.core :refer [generate-string parse-string]]
-    [clojure.java.jdbc :as jdbc]
+    [next.jdbc.date-time]
+    [next.jdbc.prepare]
+    [next.jdbc.result-set]
     [clojure.tools.logging :as log]
     [conman.core :as conman]
-    [java-time :as jt]
-    [java-time.pre-java8]
     [<<project-ns>>.config :refer [env]]
     [mount.core :refer [defstate]])
-  (:import org.postgresql.util.PGobject
-           java.sql.Array
-           clojure.lang.IPersistentMap
-           clojure.lang.IPersistentVector
-           [java.sql
-            BatchUpdateException
-            PreparedStatement])<% endifequal %><% ifequal db-type "mysql" %>
+  (:import (org.postgresql.util PGobject))<% endifequal %><% ifequal db-type "mysql" %>
   (:require
-    [clojure.java.jdbc :as jdbc]
+    [next.jdbc.date-time]
+    [next.jdbc.result-set]
     [clojure.tools.logging :as log]
     [conman.core :as conman]
-    [java-time.pre-java8 :as jt]
     [<<project-ns>>.config :refer [env]]
-    [mount.core :refer [defstate]])
-  (:import [java.sql
-            BatchUpdateException
-            PreparedStatement])<% endifequal %>)
+    [mount.core :refer [defstate]])<% endifequal %>)
 <% if embedded-db %>
 (defstate ^:dynamic *db*
           :start (conman/connect! {:jdbc-url (env :database-url)})
           :stop (conman/disconnect! *db*))
-<% else %>(defstate ^:dynamic *db*
+<% else %>
+(defstate ^:dynamic *db*
   :start (if-let [jdbc-url (env :database-url)]
            (conman/connect! {:jdbc-url jdbc-url})
            (do
@@ -44,13 +36,7 @@
   :stop (conman/disconnect! *db*))
 <% endif %>
 (conman/bind-connection *db* "sql/queries.sql")
-
 <% ifequal db-type "postgres" %>
-<% include db/src/postgres-fragment.clj %>
-<% else %>
-(extend-protocol jdbc/IResultSetReadColumn
-<% include db/src/datetime-deserializers.clj %>)
-
-(extend-protocol jdbc/ISQLValue
-<% include db/src/datetime-serializers.clj %>)
-<% endifequal %>
+<% include db/src/postgres-fragment.clj %><% else %>
+(extend-protocol next.jdbc.result-set/ReadableColumn
+  <% include db/src/datetime-deserializers.clj %>)<% endifequal %>
