424084dd0de84dd74d5190b1d0eadff23fb61deb
Matthew Molloy <whamtet@gmail.com>
2021-08-05 20:29:19 +0900

    Added +ctmx option

---
+++ src/leiningen/new/ctmx.clj
@@ -0,0 +1,14 @@
+(ns leiningen.new.ctmx
+  (:require [leiningen.new.common :refer :all]))
+
+(def ctmx-assets
+  [["{{backend-path}}/{{sanitized}}/routes/home.clj" "ctmx/src/home.clj"]])
+
+(defn ctmx-features [[assets options :as state]]
+  (println "ctmx-features" (some #{"+ctmx"} (:features options)))
+  (if (some #{"+ctmx"} (:features options))
+    [(into (remove-conflicting-assets assets "home.clj") ctmx-assets)
+     (-> options
+         (append-options :dependencies [['ctmx "1.4.3"]])
+         (assoc :ctmx true))]
+    state))
